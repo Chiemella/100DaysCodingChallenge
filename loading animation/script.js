62c6bae9163b8dfd98b3f1db14d93eb852930d66
chiemella <williamschimelaod@gmail.com>
2023-04-18 13:53:57 +0100

    initial commit

--- loading animation/script.js
+++ loading animation/script.js
@@ -1,15 +1,15 @@
-// INSERT JS HERE
+// // INSERT JS HERE
 
 
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
+// // SOCIAL PANEL JS
+// const floating_btn = document.querySelector('.floating-btn');
+// const close_btn = document.querySelector('.close-btn');
+// const social_panel_container = document.querySelector('.social-panel-container');
 
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
+// floating_btn.addEventListener('click', () => {
+// 	social_panel_container.classList.toggle('visible')
+// });
 
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
+// close_btn.addEventListener('click', () => {
+// 	social_panel_container.classList.remove('visible')
+// });
