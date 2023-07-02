b75f1bfacd5c2c415d0220f746f6761b0141a75d
chiemella <williamschiemelaod@gmail.com>
2023-07-02 19:19:41 +0100

    completed

---
+++ Auto Text Effect/app.js
@@ -0,0 +1,17 @@
+const text = document.getElementById("text");
+const prog = "We Love Programming!";
+let idx = 1;
+
+setInterval(writeText, 100);
+
+function writeText() {
+    text.innerText = prog.slice(0, idx)
+	
+	idx++;
+	
+	if(idx > prog.length){
+      idx = 1;
+    }
+
+}
+
