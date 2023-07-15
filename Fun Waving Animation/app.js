2d7c65298a962c2476b75ad64cd98bd5df1b0a96
chiemella <williamschiemelaod@gmail.com>
2023-07-15 14:47:35 +0100

    updated

---
+++ Fun Waving Animation/app.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll(".form-control input");
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) => `<span style="
+				transition-delay: ${idx * 50}ms
+			">${letter}</span>`
+    )
+    .join("");
+});
